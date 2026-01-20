0e555b01e23c523499333a535a110af0f7d3abfa
wattsb <wattsb@LHSSTUDENT-GMKP>
2026-01-20 19:34:43 +0000

    Add project files.

---
+++ turnBasedProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace turnBasedProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
